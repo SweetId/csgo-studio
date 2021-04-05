107891bace4913df40164f160eb042d679a064ed
Sid <2630750+SweetId@users.noreply.github.com>
2021-04-05 16:46:13 +0200

    add video codec

--- csgo-studio-common/ffmpeg/enums.h
+++ csgo-studio-common/ffmpeg/enums.h
@@ -2,14 +2,19 @@
 
 #include "common.h"
 
+#include <type_traits>
+
 enum class ECodec : uint32_t
 {
 	None,
 
 	// Audio Codecs
-	Mp2
+	Mp2,
 
 	// Video Codecs
+	h264,
+
+	MAX
 };
 
 enum class ESampleFormat : uint16_t
@@ -20,5 +25,7 @@ enum class ESampleFormat : uint16_t
 	S16,
 	S32,
 	Float,
-	Double
+	Double,
+
+	MAX
 };
--- csgo-studio-common/ffmpeg/private/codec_converter.h
+++ csgo-studio-common/ffmpeg/private/codec_converter.h
@@ -1,6 +1,8 @@
 #include "ffmpeg.h"
 #include "ffmpeg/codec_descriptor.h"
 
+#include <QVideoFrame>
+
 namespace ffmpeg
 {
 	static enum AVCodecID ToFFMpegCodec(ECodec codec)
@@ -8,6 +10,7 @@ namespace ffmpeg
 		switch (codec)
 		{
 		case ECodec::Mp2: return AV_CODEC_ID_MP2;
+		case ECodec::h264: return AV_CODEC_ID_H264;
 		case ECodec::None: return AV_CODEC_ID_NONE;
 		default: return AV_CODEC_ID_NONE;
 		}
@@ -36,6 +39,35 @@ namespace ffmpeg
 		case AV_SAMPLE_FMT_FLT: return ESampleFormat::Float;
 		case AV_SAMPLE_FMT_DBL: return ESampleFormat::Double;
 		default: return ESampleFormat::None;
+		}
+	}
+
+	static enum AVPixelFormat ToFFMpegFormat(QVideoFrame::PixelFormat fmt)
+	{
+		switch (fmt)
+		{
+		case QVideoFrame::PixelFormat::Format_RGB24: return AV_PIX_FMT_RGB24;
+		case QVideoFrame::PixelFormat::Format_BGR24: return AV_PIX_FMT_BGR24;
+		case QVideoFrame::PixelFormat::Format_RGB32: return AV_PIX_FMT_RGBA;
+		case QVideoFrame::PixelFormat::Format_BGRA32: return AV_PIX_FMT_BGRA;
+
+		case QVideoFrame::PixelFormat::Format_YUYV: return AV_PIX_FMT_YUYV422;
+		case QVideoFrame::PixelFormat::Format_YUV420P: return AV_PIX_FMT_YUV420P;
+		default: return AV_PIX_FMT_NONE;
+		}
+	}
+
+	static QVideoFrame::PixelFormat FromFFmpegFormat(enum AVPixelFormat fmt)
+	{
+		switch (fmt)
+		{
+		case AV_PIX_FMT_RGB24: return QVideoFrame::PixelFormat::Format_BGR24;
+		case AV_PIX_FMT_BGR24: return QVideoFrame::PixelFormat::Format_BGR24;
+		case AV_PIX_FMT_RGBA: return QVideoFrame::PixelFormat::Format_RGB32;
+		case AV_PIX_FMT_BGRA: return QVideoFrame::PixelFormat::Format_BGRA32;
+		case AV_PIX_FMT_YUYV422: return QVideoFrame::PixelFormat::Format_YUYV;
+		case AV_PIX_FMT_YUV420P: return QVideoFrame::PixelFormat::Format_YUV420P;
+		default: return QVideoFrame::PixelFormat::Format_Invalid;
 		}
 	}
 }
